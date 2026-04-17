ee2530797e78ef36661fff54b1b493a37033006d
dayana <144542256+urlunada@users.noreply.github.com>
2026-04-18 00:29:18 +0300

    Add files via upload

---
+++ HookLibrary/GlobalHook.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace HookLibrary
+{
+    public class GlobalHook
+    {
+        private IntPtr _hookID = IntPtr.Zero;
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private HookProc _proc;
+
+        // Событие для передачи данных в основное приложение
+        public event Action<int, string> KeyCaught;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public GlobalHook()
+        {
+            _proc = HookCallback;
+        }
+
+        public void Install()
+        {
+            _hookID = SetWindowsHookEx(WH_KEYBOARD_LL, _proc,
+                GetModuleHandle(System.Diagnostics.Process.GetCurrentProcess().MainModule.ModuleName), 0);
+        }
+
+        public void Uninstall()
+        {
+            if (_hookID != IntPtr.Zero)
+            {
+                UnhookWindowsHookEx(_hookID);
+                _hookID = IntPtr.Zero;
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Keys key = (Keys)vkCode;
+
+                // Отправляем данные в основное приложение
+                KeyCaught?.Invoke(vkCode, key.ToString());
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+    }
+}
---
+++ Task6 хуки/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Task6_dll
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnInstall = new Button();
+            btnUninstall = new Button();
+            lblInfo = new Label();
+            lblStatus = new Label();
+            SuspendLayout();
+            // 
+            // btnInstall
+            // 
+            btnInstall.Location = new Point(65, 108);
+            btnInstall.Name = "btnInstall";
+            btnInstall.Size = new Size(185, 34);
+            btnInstall.TabIndex = 0;
+            btnInstall.Text = "Установить хук";
+            btnInstall.UseVisualStyleBackColor = true;
+            btnInstall.Click += btnInstall_Click;
+            // 
+            // btnUninstall
+            // 
+            btnUninstall.Location = new Point(65, 217);
+            btnUninstall.Name = "btnUninstall";
+            btnUninstall.Size = new Size(185, 34);
+            btnUninstall.TabIndex = 1;
+            btnUninstall.Text = "Удалить хук";
+            btnUninstall.UseVisualStyleBackColor = true;
+            btnUninstall.Click += btnUninstall_Click;
+            // 
+            // lblInfo
+            // 
+            lblInfo.BackColor = SystemColors.ActiveCaption;
+            lblInfo.Location = new Point(298, 96);
+            lblInfo.Name = "lblInfo";
+            lblInfo.Size = new Size(220, 62);
+            lblInfo.TabIndex = 2;
+            lblInfo.Text = " ";
+            // 
+            // lblStatus
+            // 
+            lblStatus.Location = new Point(298, 222);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(233, 37);
+            lblStatus.TabIndex = 3;
+            lblStatus.Text = "Статус: Хук не установлен";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(941, 482);
+            Controls.Add(lblStatus);
+            Controls.Add(lblInfo);
+            Controls.Add(btnUninstall);
+            Controls.Add(btnInstall);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnInstall;
+        private Button btnUninstall;
+        private Label lblInfo;
+        private Label lblStatus;
+    }
+}

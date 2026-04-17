466254af0f199ee4e828036012940c7f03b1a930
dayana <144542256+urlunada@users.noreply.github.com>
2026-04-18 00:27:56 +0300

    Add files via upload

---
+++ FormulaDll/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FormulaDll
+{
+    public class Calculator
+    {
+        // Формула: вычисление гипотенузы (корень из суммы квадратов)
+        // a^2 + b^2 = c^2
+        public static double Calculate(double a, double b)
+        {
+            // Math.Sqrt - квадратный корень
+            return Math.Sqrt(a * a + b * b);
+        }
+    }
+}
---
+++ FormulaDll/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("FormulaDll")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FormulaDll")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("17437a18-c07f-4f71-9767-8f1c0b5bad80")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
